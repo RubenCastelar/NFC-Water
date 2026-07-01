e283918e28cddf3dd5016a2a8cfe12bb5ec2bb20
RubiCastelar <132750514+RubenCastelar@users.noreply.github.com>
2026-07-01 21:24:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "agua-nfc-cache-v3";
-const ASSETS = ["./", "./index.html", "./styles.css?v=2", "./app.js?v=3", "./manifest.webmanifest", "./assets/icon.svg"];
+const CACHE_NAME = "agua-nfc-cache-v6";
+const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=5", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
